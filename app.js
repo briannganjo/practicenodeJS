558b1e6338fbdeb5c086aa8afa83efff07f0861c
briannganjo <52170424+briannganjo@users.noreply.github.com>
2025-04-14 20:48:24 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ const server = http.createServer((req, res) => {
   res.end('Hello from Node.js Pipeline!');
 });
 
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
+

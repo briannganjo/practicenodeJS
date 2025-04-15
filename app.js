e6005c0ec73a0d3214ee2d7e327c6250a5ce465c
briannganjo <52170424+briannganjo@users.noreply.github.com>
2025-04-14 21:17:26 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const server = http.createServer((req, res) => {
   res.end('Hello from Node.js Pipeline!');
 });
 
-server.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server is running on port 3000');
 });
+
 
